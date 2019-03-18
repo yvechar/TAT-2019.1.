47625b01c0cf69010c95547b4cc0553879f9db35
yana vecher <yanavecher.sp@gmail.com>
2019-03-19 01:43:04 +0300

    first task another files

---
+++ task-11/task-11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_1
+{
+    /// <summary>
+    /// The Class EntryPoint provides main
+    /// </summary>
+    /// <param name="args">arguments from command line</param>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                foreach (string arg in args)
+                {
+                    SearchForConSymbols Obj = new SearchForConSymbols();
+                    Obj.SearchAndDisplay(arg);
+                }
+            }
+            catch (FormatException)
+
+            {
+                Console.WriteLine("Empty String");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
